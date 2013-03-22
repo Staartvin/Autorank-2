2d6307494e7fd71024a29de773ddeae97d6b1cc0
Vincent <mijnleraar@msn.com>
2013-03-22 17:57:38 +0100

    Added 'multiple command performing' support.

--- src/me/armar/plugins/autorank/playerchecker/result/CommandResult.java
+++ src/me/armar/plugins/autorank/playerchecker/result/CommandResult.java
@@ -1,27 +1,36 @@
 package me.armar.plugins.autorank.playerchecker.result;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 public class CommandResult extends Result {
 
-	private String command = null;
+	private List<String> commands = null;
 	private Server server = null;
 
 	@Override
-	public boolean setOptions(String[] options) {
+	public boolean setOptions(String[] commands) {
 		this.server = this.getAutorank().getServer();
-		command = options[0];
+		List<String> replace = new ArrayList<String>();
+		for (String command:commands) {
+			replace.add(command);
+		}
+		this.commands = replace;
 		return true;
 	}
 
 	@Override
 	public boolean applyResult(Player player) {
 		if (server != null) {
-			String cmd = command.replace("&p", player.getName());
-			server.dispatchCommand(server.getConsoleSender(), cmd);
+			for (String command:commands) {
+				String cmd = command.replace("&p", player.getName());
+				server.dispatchCommand(server.getConsoleSender(), cmd);
+			}
+			
 		}
 		return server != null;
 	}
-
 }
