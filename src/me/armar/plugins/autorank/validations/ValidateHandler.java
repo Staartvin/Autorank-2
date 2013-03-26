5244f6b0b5ffaacf08f31bc575b8ea5a3756095b
Vincent <mijnleraar@msn.com>
2013-03-26 18:37:17 +0100

    Updated version to 'Beta 2.1.2'

--- src/me/armar/plugins/autorank/validations/ValidateHandler.java
+++ src/me/armar/plugins/autorank/validations/ValidateHandler.java
@@ -7,16 +7,17 @@ public class ValidateHandler {
 
 	private Autorank autorank;
 	private PermissionGroupValidation permGroupValidate;
-	
+
 	public ValidateHandler(Autorank instance) {
 		this.autorank = instance;
 		permGroupValidate = new PermissionGroupValidation(instance);
 	}
-	
+
 	public boolean validateConfigGroups(SimpleYamlConfiguration config) {
-		
+
 		if (permGroupValidate.validateGroups(config) == false) {
-			autorank.getLogger().severe("There are invalid groups defined in the config!");
+			autorank.getLogger().severe(
+					"There are invalid groups defined in the config!");
 			autorank.getLogger().severe("Check your config!");
 			return false;
 		} else {
