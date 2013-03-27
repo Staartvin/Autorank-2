8b41fa521c4f559fbb1100220c3a5679837eb737
Armarr <deprins.r@gmail.com>
2013-03-27 20:08:15 +0100

    Added EffectResult

---
+++ src/me/armar/plugins/autorank/playerchecker/result/EffectResult.java
@@ -0,0 +1,31 @@
+package me.armar.plugins.autorank.playerchecker.result;
+
+import org.bukkit.Effect;
+import org.bukkit.entity.Player;
+
+public class EffectResult extends Result{
+	
+	private Effect effect;
+	private int data;
+
+	@Override
+	public boolean setOptions(String[] options) {
+		if (options.length > 0){
+			effect = Effect.valueOf(options[0]);
+		}
+		if (options.length > 1){
+			data = Integer.parseInt(options[1]);
+		}
+			
+		return effect != null;
+	}
+
+	@Override
+	public boolean applyResult(Player player) {
+		if(effect != null){
+			player.getWorld().playEffect(player.getLocation(), effect, data);
+		}
+		return effect != null;
+	}
+
+}
