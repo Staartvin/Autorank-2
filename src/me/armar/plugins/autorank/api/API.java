30d98411c207c34e6ba0d2e1bf6a1e8f7bab07da
Vincent <mijnleraar@msn.com>
2013-08-05 13:52:16 +0200

    Started working on API class

---
+++ src/me/armar/plugins/autorank/api/API.java
@@ -0,0 +1,46 @@
+package me.armar.plugins.autorank.api;
+
+import me.armar.plugins.autorank.Autorank;
+import me.armar.plugins.autorank.language.Language;
+import me.armar.plugins.autorank.language.LanguageHandler;
+
+/**
+ * <b>Autorank's API class:</b>
+ * <p>
+ * You, as a developer, can you use this class to get data from players or data about groups.
+ * The API is never finished and if you want to see something added, tell us!
+ * <p>
+ * @author Staartvin
+ *
+ */
+public class API {
+
+	private Autorank plugin;
+	
+	public API(Autorank instance) {
+		plugin = instance;
+	}
+	
+	/**
+	 * Gets the local play time (playtime on this server) of a player.
+	 * <p>
+	 * @param playerName player to check for.
+	 * @return play time of a player. 0 when has never played before.
+	 */
+	public int getLocalPlayTime(String playerName) {
+		return plugin.getLocalTime(playerName);
+	}
+	
+	/** 
+	 * Gets the selected language
+	 * @return language as {@link me.armar.plugins.language.Language} 
+	 */
+	public Language getSelectedLanguage() {
+		return LanguageHandler.getLanguage(); 
+	}
+	
+	public String getMySQLDatabase() {
+		return plugin.getMySQLWrapper().getDatabaseName();
+	}
+	
+}
