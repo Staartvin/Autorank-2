cf22d2a9a31cb0c543edac90f29f6e21aae327ec
Vincent <mijnleraar@msn.com>
2013-08-07 22:25:35 +0200

    Fix compatibility with old config

--- src/me/armar/plugins/autorank/config/ConfigHandler.java
+++ src/me/armar/plugins/autorank/config/ConfigHandler.java
@@ -6,57 +6,71 @@ import me.armar.plugins.autorank.Autorank;
 import me.armar.plugins.autorank.data.SimpleYamlConfiguration;
 
 /**
- * This class handles has all methods to get data from the config. This is now handled by every class seperately, but should be organised soon.
+ * This class handles has all methods to get data from the config. This is now
+ * handled by every class seperately, but should be organised soon.
  * 
  * @author Staartvin
- *
+ * 
  */
 public class ConfigHandler {
 
 	private Autorank plugin;
 	private SimpleYamlConfiguration config;
-	
+
 	public ConfigHandler(Autorank instance) {
 		plugin = instance;
 		this.config = plugin.getAdvancedConfig();
 	}
-	
+
 	/**
 	 * Gets whether a requirement is optional for a certain group
+	 * 
 	 * @param requirement
 	 * @param group
 	 * @return true if optional; false otherwise
 	 */
 	public boolean isOptional(String requirement, String group) {
-		boolean optional = config.getBoolean("ranks." + group + ".requirements." + requirement + ".options.optional", false);
-		
+		boolean optional = config.getBoolean("ranks." + group
+				+ ".requirements." + requirement + ".options.optional", false);
+
 		return optional;
 	}
-	
+
 	public Set<String> getRequirements(String group) {
-		Set<String> requirements = config.getConfigurationSection("ranks." + group + ".requirements").getKeys(false);
-		
+		Set<String> requirements = config.getConfigurationSection(
+				"ranks." + group + ".requirements").getKeys(false);
+
 		return requirements;
 	}
-	
+
 	public Set<String> getResults(String group) {
-		Set<String> results = config.getConfigurationSection("ranks." + group + ".results").getKeys(false);
-		
+		Set<String> results = config.getConfigurationSection(
+				"ranks." + group + ".results").getKeys(false);
+
 		return results;
 	}
-	
+
 	public Set<String> getRanks() {
 		return config.getConfigurationSection("ranks").getKeys(false);
 	}
-	
-	public Object getRequirement(String requirement, String group) {
-		return config.get("ranks." + group + ".requirements." + requirement + ".value");
+
+	public String getRequirement(String requirement, String group) {
+
+		// Correct config
+		String result;
+		result = (config.get("ranks." + group + ".requirements." + requirement
+				+ ".value") != null) ? config.get(
+				"ranks." + group + ".requirements." + requirement + ".value")
+				.toString() : config.getString(
+				"ranks." + group + ".requirements." + requirement).toString();
+
+		return result;
 	}
-	
-	public Object getResult(String result, String group) {
-		return config.get("ranks." + group + ".results." + result);
+
+	public String getResult(String result, String group) {
+		return config.get("ranks." + group + ".results." + result).toString();
 	}
-	
+
 	public String getRankChange(String group) {
 		return config.getString("ranks." + group + ".results.rank change");
 	}
