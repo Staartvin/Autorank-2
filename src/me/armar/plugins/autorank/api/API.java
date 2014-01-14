7a0b6313388a745eab17c099a4db97bad7d15573
Staartvin <mijnleraar@msn.com>
2014-01-14 12:15:36 +0100

    Implement new methods for getting the permission groups (even though you can do this with Vault)
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/api/API.java
+++ src/me/armar/plugins/autorank/api/API.java
@@ -68,7 +68,7 @@ public class API {
 	 * config file.
 	 * 
 	 * @param player Player to get the requirements from.
-	 * @return a list of requirement. An empty list when none are found.
+	 * @return a list of requirements; An empty list when none are found.
 	 */
 	public List<Requirement> getAllRequirements(final Player player) {
 		final Map<RankChange, List<Requirement>> failed = plugin
@@ -102,5 +102,53 @@ public class API {
 		}
 
 		return failedRequirements;
+	}
+	
+	/**
+	 * Gets the primary permissions group of a player.
+	 * @param player Player to get the primary group of
+	 * @return Name of the group that appears first.
+	 */
+	public String getPrimaryGroup(Player player) {
+		List<String> groups = getPermissionGroups(player);
+		
+		if (groups.size() < 1) {
+			throw new IllegalArgumentException("Groups of player '" + player.getName() + "' are empty.");
+		}
+		
+		return groups.get(0);
+	}
+	
+	/**
+	 * Gets the permission groups a player is part of.
+	 * @param player Player to get the groups of
+	 * @return A list of permission groups
+	 */
+	public List<String> getPermissionGroups(Player player) {
+		String[] groups = plugin.getPermPlugHandler().getPermissionPlugin().getPlayerGroups(player);
+		
+		List<String> permGroups = new ArrayList<String>();
+		
+		// Convert array into list
+		for (String group: groups) {
+			permGroups.add(group);
+		}
+		
+		return permGroups;
+	}
+	
+	/**
+	 * Gets the permission group that the player will be ranked up to after
+	 * he completes all requirements.
+	 * <p>
+	 * <b>NOTE:</b> This does not mean the player will always be ranked up to this group. 
+	 * If a requirement has its own <i>'rank change'</i> result, the player
+	 * will be ranked up to that group and not the 'global results' group.
+	 * 
+	 * @param player Player to get the next rank up for.
+	 * @return The name of the group the player will be ranked to; null when no rank up.
+	 */
+	public String getNextRankup(Player player) {
+		return plugin.getPlayerChecker().getNextRankup(player);
 	}
 }
