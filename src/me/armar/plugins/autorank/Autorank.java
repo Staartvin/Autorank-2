adfea0a964eb06683a4f11bf7db46020c53a8b69
Staartvin <mijnleraar@msn.com>
2014-01-21 18:54:28 +0100

    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/Autorank.java
+++ src/me/armar/plugins/autorank/Autorank.java
@@ -67,7 +67,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Autorank extends JavaPlugin {
 
-	// Will this work? 2
+	// Almost correct
 	private Leaderboard leaderboard;
 	private Playtimes playtimes;
 	private PlayerChecker playerChecker;
