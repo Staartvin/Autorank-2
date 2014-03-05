4c74286c84132eadd68b9d06c7557c6b4c348085
Staartvin <mijnleraar@msn.com>
2014-03-05 17:31:30 +0100

    Improve addon manager.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/addons/AddOnManager.java
+++ src/me/armar/plugins/autorank/addons/AddOnManager.java
@@ -1,12 +1,11 @@
-/**
- * 
- */
 package me.armar.plugins.autorank.addons;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Set;
 
 import me.armar.plugins.autorank.Autorank;
+
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * This will class will take care of all the handling of addons for Autorank.
@@ -20,31 +19,53 @@ public class AddOnManager {
 
 	private Autorank plugin;
 	
-	// Keep track of all loaded addons
-	private List<String> loadedAddons = new ArrayList<String>();
+	private HashMap<String, JavaPlugin> loadedAddons = new HashMap<String, JavaPlugin>();
 	
+	/**
+	 * AddOnManager handlers all actions between addons and Autorank. You can access this AddOnManager on any time you like.
+	 * @param instance instance of the main class of Autorank.
+	 */
 	public AddOnManager(Autorank instance) {
 		plugin = instance;
 	}
 	
-	public List<String> getLoadedAddons() {
-		return loadedAddons;
+	/**
+	 * Get all loaded addons.
+	 * @return a list of loaded addons
+	 */
+	public Set<String> getLoadedAddons() {
+		return loadedAddons.keySet();
 	}
 	
+	/**
+	 * Check to see if a certain addon is loaded
+	 * @param addonName name of the addon
+	 * @return true if loaded, false otherwise.
+	 */
 	public boolean isAddonLoaded(String addonName) {
-		return loadedAddons.contains(addonName);
+		return loadedAddons.containsKey(addonName);
 	}
 	
-	public void loadAddon(String addonName) {
+	/**
+	 * Load an addon so that it can be accessed later on.
+	 * @param addonName name of the addon
+	 * @param addon Main class of the addon
+	 */
+	public void loadAddon(String addonName, JavaPlugin addon) {
 		// Register addon
 		if (isAddonLoaded(addonName)) return;
 		
-		loadedAddons.add(addonName);
+		loadedAddons.put(addonName, addon);
 		
 		// Announce addon
 		plugin.getLogger().info("Loaded addon " + addonName);
 	}
 	
+	/**
+	 * Unload an addon so its reference is removed and can no longer be accessed. <br>
+	 * <b>NOTE:</b> Autorank will not unload the addon itself. It will only remove the reference to it.
+	 * @param addonName
+	 */
 	public void unloadAddon(String addonName) {
 		if (!isAddonLoaded(addonName)) return;
 		
@@ -54,12 +75,23 @@ public class AddOnManager {
 		plugin.getLogger().info("Unloaded addon " + addonName);
 	}
 	
+	/**
+	 * Get the main class of an addon
+	 * @param addonName name of the addon
+	 * @return main class of the addon, or null if non-existent.
+	 */
+	public JavaPlugin getLoadedAddon(String addonName) {
+		if (!isAddonLoaded(addonName)) return null;
+		
+		return loadedAddons.get(addonName);
+	}
+	
+	/**
+	 * Unload all addons that are currently loaded.
+	 */
 	public void unloadAllAddons() {
-		for (String addon: loadedAddons) {
+		for (String addon: loadedAddons.keySet()) {
 			unloadAddon(addon);
 		}
 	}
-	
-	
-	
 }
