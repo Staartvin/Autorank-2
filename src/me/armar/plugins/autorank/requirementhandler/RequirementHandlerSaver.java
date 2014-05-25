9aaf1d321a897dbaa43b05423fe1a3df4fb8f4d8
Staartvin <mijnleraar@msn.com>
2014-05-25 19:13:48 +0200

    Add RequirementHandlerSaver that saves the playerdata.yml every minute.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

---
+++ src/me/armar/plugins/autorank/requirementhandler/RequirementHandlerSaver.java
@@ -0,0 +1,30 @@
+package me.armar.plugins.autorank.requirementhandler;
+
+/**
+ * Simple runnable task that saves the playerdata.yml every minute.
+ * <p>
+ * Date created:  17:59:49
+ * 22 mei 2014
+ * @author Staartvin
+ *
+ */
+public class RequirementHandlerSaver implements Runnable {
+
+	
+	private RequirementHandler requirementHandler;
+	
+	public RequirementHandlerSaver(RequirementHandler reqHandler) {
+		requirementHandler = reqHandler;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		requirementHandler.saveConfig();
+		
+	}
+
+}
