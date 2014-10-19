e082d48e033605f6feb37b6f09ad34e176345aea
Staartvin <mijnleraar@msn.com>
2014-10-19 17:02:49 +0200

    Obsolete imports.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/listeners/PlayerJoinListener.java
+++ src/me/armar/plugins/autorank/listeners/PlayerJoinListener.java
@@ -2,18 +2,12 @@ package me.armar.plugins.autorank.listeners;
 
 import me.armar.plugins.autorank.Autorank;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
 
 /**
  * This listener will listen to players joining and send them a message when an
