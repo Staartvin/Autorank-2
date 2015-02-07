1ea1e15d721ab5e846b100fbc203843fbd3ef6b4
Staartvin <mijnleraar@msn.com>
2015-02-07 21:28:55 +0100

    Update UltimateCore handler for 2.0.0
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/hooks/ultimatecoreapi/UltimateCoreHandler.java
+++ src/me/armar/plugins/autorank/hooks/ultimatecoreapi/UltimateCoreHandler.java
@@ -9,129 +9,133 @@ import org.bukkit.plugin.Plugin;
  * Handles all connections with Ultimate Core
  * <p>
  * Date created: 21:02:20 15 mrt. 2014
- *
+ * 
  * @author Staartvin
- *
+ * 
  */
 public class UltimateCoreHandler implements DependencyHandler {
 
-    private Plugin api;
-    private final Autorank instance;
-    private Object UC;
+	private Plugin api;
+	private final Autorank instance;
 
-    public UltimateCoreHandler(final Autorank instance) {
-        this.instance = instance;
-    }
+	public UltimateCoreHandler(final Autorank instance) {
+		this.instance = instance;
+	}
 
-    /* (non-Javadoc)
-     * @see me.armar.plugins.autorank.hooks.DependencyHandler#get()
-     */
-    @Override
-    public Plugin get() {
-        final Plugin plugin = this.instance.getServer().getPluginManager()
-                .getPlugin("UltimateCore");
+	/* (non-Javadoc)
+	 * @see me.armar.plugins.autorank.hooks.DependencyHandler#get()
+	 */
+	@Override
+	public Plugin get() {
+		final Plugin plugin = this.instance.getServer().getPluginManager()
+				.getPlugin("UltimateCore");
 
-        // UltimateCore may not be loaded
-        try { //Avoid ClassNotFound
-            if (plugin != null && plugin instanceof Bammerbom.UltimateCore.UltimateCore) {
-                return plugin;
-            }
-        } catch (Exception ex) {
-        }
-        try { //Avoid ClassNotFound
-            if (plugin != null && plugin instanceof bammerbom.ultimatecore.bukkit.UltimateCore) {
-                return plugin;
-            }
-        } catch (Exception ex) {
-        }
+		// UltimateCore may not be loaded
+		try { //Avoid ClassNotFound
+			if (plugin != null
+					&& plugin instanceof bammerbom.ultimatecore.UltimateCore) {
+				return plugin;
+			}
+		} catch (Exception ex) {
+		}
+		try { //Avoid ClassNotFound
+			if (plugin != null
+					&& plugin instanceof bammerbom.ultimatecore.bukkit.UltimateCore) {
+				return plugin;
+			}
+		} catch (Exception ex) {
+		}
 
-        return plugin;
-    }
+		return plugin;
+	}
 
-    /* (non-Javadoc)
-     * @see me.armar.plugins.autorank.hooks.DependencyHandler#isAvailable()
-     */
-    @Override
-    public boolean isAvailable() {
-        return api != null;
-    }
+	/* (non-Javadoc)
+	 * @see me.armar.plugins.autorank.hooks.DependencyHandler#isAvailable()
+	 */
+	@Override
+	public boolean isAvailable() {
+		return api != null;
+	}
 
-    /* (non-Javadoc)
-     * @see me.armar.plugins.autorank.hooks.DependencyHandler#isInstalled()
-     */
-    @Override
-    public boolean isInstalled() {
-        final Plugin plugin = (Plugin) get();
-        return plugin != null && plugin.isEnabled();
-    }
+	/* (non-Javadoc)
+	 * @see me.armar.plugins.autorank.hooks.DependencyHandler#isInstalled()
+	 */
+	@Override
+	public boolean isInstalled() {
+		final Plugin plugin = (Plugin) get();
+		return plugin != null && plugin.isEnabled();
+	}
 
-    /* (non-Javadoc)
-     * @see me.armar.plugins.autorank.hooks.DependencyHandler#setup()
-     */
-    @Override
-    public boolean setup(final boolean verbose) {
-        if (!isInstalled()) {
-            if (verbose) {
-                instance.getLogger().info("UltimateCore has not been found!");
-            }
-            return false;
-        } else {
-            api = get();
+	/* (non-Javadoc)
+	 * @see me.armar.plugins.autorank.hooks.DependencyHandler#setup()
+	 */
+	@Override
+	public boolean setup(final boolean verbose) {
+		if (!isInstalled()) {
+			if (verbose) {
+				instance.getLogger().info("UltimateCore has not been found!");
+			}
+			return false;
+		} else {
+			api = get();
 
-            if (api != null) {
-                if (verbose) {
-                    instance.getLogger().info(
-                            "UltimateCore has been found and can be used!");
-                }
-                return true;
-            } else {
-                if (verbose) {
-                    instance.getLogger().info(
-                            "UltimateCore has been found but cannot be used!");
-                }
-                return false;
-            }
-        }
-    }
+			if (api != null) {
 
-    public Integer getVersion() {
-        if (api.getDescription().getVersion().startsWith("1")) {
-            return 1;
-        }
-        if (api.getDescription().getVersion().startsWith("2")) {
-            return 2;
-        }
-        return null;
-    }
+				if (verbose) {
+					instance.getLogger().info(
+							"UltimateCore has been found and can be used!");
+				}
+				return true;
+			} else {
+				if (verbose) {
+					instance.getLogger().info(
+							"UltimateCore has been found but cannot be used!");
+				}
+				return false;
+			}
+		}
+	}
 
-    public boolean isAFK(final Player player) {
-        if (!isAvailable()) {
-            return false;
-        }
+	public Integer getVersion() {
+		if (api.getDescription().getVersion().startsWith("1")) {
+			return 1;
+		}
+		if (api.getDescription().getVersion().startsWith("2")) {
+			return 2;
+		}
+		return null;
+	}
 
-        if (!instance.getConfigHandler().useAFKIntegration()) {
-            return false;
-        }
+	public boolean isAFK(final Player player) {
+		if (!isAvailable()) {
+			return false;
+		}
 
-        if (getVersion().equals(1)) {
-            final Bammerbom.UltimateCore.API.UCplayer user = Bammerbom.UltimateCore.API.UC.getPlayer(player);
+		if (!instance.getConfigHandler().useAFKIntegration()) {
+			return false;
+		}
 
-            if (user == null) {
-                return false;
-            }
+		if (getVersion().equals(1)) {
+			final bammerbom.ultimatecore.bukkit.api.UPlayer user = bammerbom.ultimatecore.bukkit.api.UC
+					.getPlayer(player);
 
-            return user.isAFK();
-        }
-        if (getVersion().equals(2)) {
-            final bammerbom.ultimatecore.bukkit.api.UPlayer user = bammerbom.ultimatecore.bukkit.api.UC.getPlayer(player);
+			if (user == null) {
+				return false;
+			}
 
-            if (user == null) {
-                return false;
-            }
+			return user.isAfk();
+		}
+		if (getVersion().equals(2)) {
+			final bammerbom.ultimatecore.bukkit.api.UPlayer user = bammerbom.ultimatecore.bukkit.api.UC
+					.getPlayer(player);
 
-            return user.isAfk();
-        }
-        return false;
+			if (user == null) {
+				return false;
+			}
 
-    }
+			return user.isAfk();
+		}
+		return false;
+
+	}
 }
