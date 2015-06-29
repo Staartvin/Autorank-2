0847f70b3dd8048143c8bdef3a70a3560b38704e
Staartvin <mijnleraar@msn.com>
2015-06-29 16:15:04 +0200

    Add method to get food names for custom 'food eaten' stat.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/util/AutorankTools.java
+++ src/me/armar/plugins/autorank/util/AutorankTools.java
@@ -390,4 +390,90 @@ public class AutorankTools {
 
 		return count >= amount;
 	}
+
+	/**
+	 * Get the name of this food item.
+	 * 
+	 * @param item ItemStack to get the name of.
+	 * @return Name of food, or null if not a valid food item.
+	 */
+	public static String getFoodName(ItemStack item) {
+		// Returns null if not a valid food item
+		// Got Materials from https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Material.html
+		
+		switch (item.getType()) {
+		case APPLE:
+			return "APPLE";
+		case BAKED_POTATO:
+			return "BAKED_POTATO";
+		case BREAD:
+			return "BREAD";
+		case CAKE_BLOCK: // not working atm
+			return "CAKE_BLOCK";
+		case CARROT_ITEM:
+			return "CARROT_ITEM";
+		case COOKED_CHICKEN:
+			return "COOKED_CHICKEN";
+		case COOKED_FISH: {
+			if (item.getDurability() == (short) 1) {
+				return "COOKED_SALMON";
+			}
+			return "COOKED_FISH";
+		}
+		case COOKED_MUTTON:
+			return "COOKED_MUTTON";
+		case GRILLED_PORK:
+			return "GRILLED_PORK";
+		case COOKED_RABBIT:
+			return "COOKED_RABBIT";
+		case COOKIE:
+			return "COOKIE";
+		case GOLDEN_APPLE: {
+			if (item.getDurability() == (short) 1) {
+				return "ENCHANTED_GOLDEN_APPLE";
+			}
+			return "GOLDEN_APPLE";
+		}
+		case GOLDEN_CARROT:
+			return "GOLDEN_CARROT";
+		case MELON:
+			return "MELON";
+		case MUSHROOM_SOUP:
+			return "MUSHROOM_SOUP";
+		case RABBIT_STEW:
+			return "RABBIT_STEW";
+		case RAW_BEEF:
+			return "RAW_BEEF";
+		case RAW_CHICKEN:
+			return "RAW_CHICKEN";
+		case RAW_FISH: {
+			if (item.getDurability() == (short) 1) {
+				return "RAW_SALMON";
+			} else if (item.getDurability() == (short) 2) {
+				return "CLOWNFISH";
+			} else if (item.getDurability() == (short) 3) {
+				return "PUFFERFISH";
+			}
+			return "RAW_FISH";
+		}
+		case POISONOUS_POTATO:
+			return "POISONOUS_POTATO";
+		case POTATO:
+			return "POTATO";
+		case PUMPKIN_PIE:
+			return "PUMPKIN_PIE";
+		case MUTTON:
+			return "MUTTON"; // raw
+		case COOKED_BEEF:
+			return "COOKED_BEEF";
+		case RABBIT:
+			return "RABBIT";
+		case ROTTEN_FLESH:
+			return "ROTTEN_FLESH";
+		case SPIDER_EYE:
+			return "SPIDER_EYE";
+		default:
+			return null;
+		}
+	}
 }
