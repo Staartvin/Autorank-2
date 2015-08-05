2bb298bdbc76dd94de6a326d180ae49b802d4c34
Staartvin <mijnleraar@msn.com>
2015-08-05 22:38:50 +0200

    Add a manager that handles all things of ChangeGroup
    Signed-off-by: Staartvin <mijnleraar@msn.com>

---
+++ src/me/armar/plugins/autorank/rankbuilder/ChangeGroupManager.java
@@ -0,0 +1,139 @@
+package me.armar.plugins.autorank.rankbuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import me.armar.plugins.autorank.Autorank;
+
+/**
+ * Handles all things that have to do with change groups checking
+ * <p>
+ * Date created: 16:32:49 5 aug. 2015
+ * 
+ * @author Staartvin
+ * 
+ */
+public class ChangeGroupManager {
+
+	private Autorank plugin;
+	private ChangeGroupBuilder builder;
+
+	// The String is a name of the group, used to get the change groups from that group.
+	// The List contains all change groups that this parent group has.
+	// One ChangeGroup class represents one path to take in the group
+	private HashMap<String, List<ChangeGroup>> changeGroups = new HashMap<String, List<ChangeGroup>>();
+
+	public ChangeGroupManager(Autorank plugin) {
+		this.plugin = plugin;
+		setBuilder(new ChangeGroupBuilder(plugin));
+	}
+
+	public ChangeGroupBuilder getBuilder() {
+		return builder;
+	}
+
+	public void setBuilder(ChangeGroupBuilder builder) {
+		this.builder = builder;
+	}
+
+	public void initialiseFromConfigs() {
+
+		// Clear before starting
+		changeGroups.clear();
+
+		if (plugin.getConfigHandler().useAdvancedConfig()) {
+			builder.initialiseChangeGroups(false, plugin.getAdvancedConfig(),
+					changeGroups);
+		} else {
+			builder.initialiseChangeGroups(true, plugin.getSimpleConfig(),
+					changeGroups);
+		}
+
+		for (String message : debugChangeGroups(true)) {
+			plugin.debugMessage(message);
+		}
+
+	}
+
+	// Returns a list of strings that can be printed out one for one to get the debug
+	public List<String> debugChangeGroups(boolean deepInfo) {
+
+		List<String> messages = new ArrayList<String>();
+
+		messages.add(" ------------------- ChangeGroup debug info ------------------- ");
+
+		for (Entry<String, List<ChangeGroup>> entry : changeGroups.entrySet()) {
+			String groupName = entry.getKey();
+			List<ChangeGroup> groups = entry.getValue();
+
+			messages.add("Group: " + groupName);
+
+			for (ChangeGroup group : groups) {
+				messages.add("- " + group.getInternalGroup());
+
+				// Provide more info
+				if (deepInfo) {
+					messages.add("    - " + group.getRequirements().size()
+							+ " requirements");
+					messages.add("    - " + group.getResults().size()
+							+ " results");
+				}
+			}
+
+			messages.add("----------------------------");
+
+		}
+
+		return messages;
+	}
+
+	public List<ChangeGroup> getChangeGroups(String groupName) {
+		// return empty list if nothing found
+		if (!changeGroups.containsKey(groupName)) return new ArrayList<ChangeGroup>();
+		
+		return changeGroups.get(groupName);
+	}
+
+	/**
+	 * Get the changegroup that corresponds to the given 'chosenpath' variable.
+	 * 
+	 * @param parentGroup Group that the ChangeGroup belongs to
+	 * @param chosenPath The internal name of the ChangeGroup
+	 * @return
+	 */
+	public ChangeGroup matchChangeGroup(String parentGroup, String chosenPath) {
+		List<ChangeGroup> changeGroup = this.getChangeGroups(parentGroup);
+
+		if (changeGroup == null)
+			return null;
+
+		// If there is only one change group, the player's choice doesn't matter.
+		if (changeGroup.size() == 1) {
+			return changeGroup.get(0);
+		}
+
+		for (ChangeGroup change : changeGroup) {
+			String internalName = change.getInternalGroup();
+
+			if (internalName.equals(chosenPath)) {
+				return change;
+			}
+		}
+		return null;
+	}
+	
+	public ChangeGroup matchChangeGroupFromDisplayName(String parentGroup, String displayName) {
+		List<ChangeGroup> changeGroupList = this.getChangeGroups(parentGroup);
+		
+		for (ChangeGroup group: changeGroupList) {
+			if (group.getDisplayName().toLowerCase().equals(displayName)) {
+				return group;
+			}
+		}
+		
+		return null;
+	}
+
+}
