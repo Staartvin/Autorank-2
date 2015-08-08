c81a368545983a37e006edac61b4f103a7d8c807
Staartvin <mijnleraar@msn.com>
2015-08-08 18:13:58 +0200

    Return primary group as they are specified in the config, so bPerms works as well.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

--- src/me/armar/plugins/autorank/permissions/PermissionsPluginManager.java
+++ src/me/armar/plugins/autorank/permissions/PermissionsPluginManager.java
@@ -86,8 +86,18 @@ public class PermissionsPluginManager {
 	public String getPrimaryGroup(Player player) {
 		// All groups of the player
 		String[] groups = this.getPermissionPlugin().getPlayerGroups(player);
+		// All ranks defined in the config
+		Set<String> ranks = plugin.getConfigHandler().getRanks();
 
 		if (groups.length == 1) {
+
+			// Match with correct rank from config file.
+			for (String rank : ranks) {
+				if (rank.equalsIgnoreCase(groups[0])) {
+					return rank;
+				}
+			}
+
 			return groups[0];
 		}
 
@@ -95,15 +105,13 @@ public class PermissionsPluginManager {
 		if (groups.length == 0)
 			return null;
 
-		// All ranks defined in the config
-		Set<String> ranks = plugin.getConfigHandler().getRanks();
-
 		for (String group : groups) {
 			// Check for every group if it is defined in the config -> if so, it probably is the primary group
 
 			for (String rank : ranks) {
 				if (group.equalsIgnoreCase(rank)) {
-					return group;
+					// Return rank name as in config
+					return rank;
 				}
 			}
 		}
