828262747450869ff466fee01b60a0dab30587fb
Staartvin <mijnleraar@msn.com>
2015-09-07 21:34:09 +0200

    Add UUIDRefresher that updates UUIDs automatically.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

---
+++ src/me/armar/plugins/autorank/util/uuid/UUIDRefresher.java
@@ -0,0 +1,132 @@
+package me.armar.plugins.autorank.util.uuid;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
+
+import me.armar.plugins.autorank.Autorank;
+import me.armar.plugins.autorank.util.AutorankTools;
+import me.armar.plugins.autorank.util.AutorankTools.Time;
+
+/**
+ * This class is run on startup of the server to refresh all users (if they are
+ * outdated). <br>
+ * It will run a task that is async that will update all users.
+ * <p>
+ * Date created: 20:02:53 2 sep. 2015
+ * 
+ * @author Staartvin
+ * 
+ */
+public class UUIDRefresher implements Runnable {
+
+	// Whether this task is running
+	public static boolean isRunning = false;
+
+	private Autorank plugin;
+
+	public UUIDRefresher(Autorank plugin) {
+		this.plugin = plugin;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		isRunning = true;
+
+		// Get a list of all uuid keys.
+		List<UUID> uuids = plugin.getPlaytimes().getUUIDKeys();
+
+		List<UUID> notOutdated = new ArrayList<UUID>();
+		
+		plugin.debugMessage("Starting up refreshing uuids...");
+		plugin.debugMessage("Setup will take " + AutorankTools.timeToString(uuids.size() / 110, Time.SECONDS));
+
+		for (int i=0;i<uuids.size();i++) {
+			
+			//System.out.println(i%100);
+			
+			// Show progress every 1000
+			if (i % 1000 == 0) {
+				//System.out.println("I: " + i);
+				plugin.debugMessage("Setup progress: " + i + "/" + uuids.size());
+			}
+			
+			
+			UUID uuid = uuids.get(i);
+			
+			String playerName = plugin.getUUIDStorage().getPlayerName(uuid);
+
+			//System.out.println("Count: " + i + "/" + uuids.size());
+			
+			if (playerName == null) {
+				continue;
+			}
+		
+			if (!plugin.getUUIDStorage().isOutdated(playerName)) {
+				// Not outdated, so we don't have to check it.
+				notOutdated.add(uuid);
+			}
+		}
+
+		// Remove all not outdated uuids from the list to check.
+		for (UUID uuid : notOutdated) {
+			uuids.remove(uuid);
+		}
+		
+		plugin.debugMessage("Setup finished!");
+
+		int size = uuids.size();
+		
+		// This will take longer than an hour, abort the refreshing and let it only update the value requested.
+		if (size > 1500) {
+			plugin.debugMessage("Tried refreshing uuids, but this will take over an hour. Aborting instead.");
+			plugin.debugMessage("Autorank will manually update a UUID when a player logs in.");
+			return;
+		}
+
+		// Time spent in seconds to refresh all.
+		int timeSpent = 0;
+		double count = 0;
+
+		if (size < 600) {
+			// Rougly one second per item
+			timeSpent = size;
+		}
+
+		// Can only request 600 per 10 minutes.
+		if (size > 600) {
+			count = size / 500.0;
+
+			int countFloored = (int) Math.floor(count);
+
+			// First 10 minutes + 20 minutes for each extra loop
+			timeSpent = 600 + (countFloored - 1) * 1200;
+
+			double dif = count - countFloored;
+
+			// Add remaining stuff. 
+			timeSpent += 600 + dif * 500;
+		}
+
+		plugin.debugMessage("Refreshing "
+				+ size
+				+ " uuids, assuming all uuids are outdated, this will take at max. about");
+		plugin.debugMessage(AutorankTools.timeToString(timeSpent, Time.SECONDS));
+
+		Map<UUID, String> names = UUIDManager.getPlayers(uuids);
+
+		for (Entry<UUID, String> entry : names.entrySet()) {
+			String name = entry.getValue();
+			UUID uuid = entry.getKey();
+
+			plugin.getUUIDStorage().storeUUID(name, uuid);
+		}
+
+	}
+
+}
