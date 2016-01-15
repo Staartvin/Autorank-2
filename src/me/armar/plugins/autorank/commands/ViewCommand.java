cf6eeaa51ea95c1f67354c3c22f3cadc3e37df13
Staartvin <mijnleraar@msn.com>
2016-01-15 21:59:10 +0100

    Clarify /ar view messages.
    Clarify messages when no paths were found or the correct name was not
    supplied.

--- src/me/armar/plugins/autorank/commands/ViewCommand.java
+++ src/me/armar/plugins/autorank/commands/ViewCommand.java
@@ -27,8 +27,7 @@ public class ViewCommand extends AutorankCommand {
 	}
 
 	@Override
-	public boolean onCommand(final CommandSender sender, final Command cmd,
-			final String label, final String[] args) {
+	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
 
 		// This command will give a preview of a certain path of ranking.
 		if (!plugin.getCommandsManager().hasPermission("autorank.view", sender)) {
@@ -36,14 +35,12 @@ public class ViewCommand extends AutorankCommand {
 		}
 
 		if (args.length < 2) {
-			sender.sendMessage(Lang.INVALID_FORMAT
-					.getConfigValue("/ar view <path name> or /ar view list"));
+			sender.sendMessage(Lang.INVALID_FORMAT.getConfigValue("/ar view <path name> or /ar view list"));
 			return true;
 		}
 
 		if (!(sender instanceof Player)) {
-			sender.sendMessage(Lang.YOU_ARE_A_ROBOT
-					.getConfigValue("you can't view ranking paths of players, silly.."));
+			sender.sendMessage(Lang.YOU_ARE_A_ROBOT.getConfigValue("you can't view ranking paths of players, silly.."));
 			return true;
 		}
 
@@ -54,59 +51,63 @@ public class ViewCommand extends AutorankCommand {
 		final String groupName = plugin.getAPI().getPrimaryGroup(player);
 
 		if (pathName.equals("list")) {
+			
+			List<ChangeGroup> groups = plugin.getPlayerChecker().getChangeGroupManager().getChangeGroups(groupName);
+
+			if (groups.isEmpty()) {
+				sender.sendMessage(ChatColor.RED + "There are currently no paths to choose from.");
+				return true;
+			}
+			
 			sender.sendMessage(ChatColor.GREEN + "You can choose these paths: ");
 
-			final String pathsString = AutorankTools
-					.createStringFromList(plugin.getPlayerChecker()
-							.getChangeGroupManager().getChangeGroups(groupName));
+			
+			final String pathsString = AutorankTools.createStringFromList(groups);
 			sender.sendMessage(ChatColor.WHITE + pathsString);
 			return true;
 		}
 
-		final ChangeGroup changeGroup = plugin
-				.getPlayerChecker()
-				.getChangeGroupManager()
-				.matchChangeGroupFromDisplayName(groupName,
-						pathName.toLowerCase());
+		final ChangeGroup changeGroup = plugin.getPlayerChecker().getChangeGroupManager()
+				.matchChangeGroupFromDisplayName(groupName, pathName.toLowerCase());
 
 		if (changeGroup == null) {
-			sender.sendMessage(ChatColor.RED
-					+ "There was no ranking path found.");
+			sender.sendMessage(ChatColor.RED + "There was no ranking path with that name found.");
 			return true;
 		}
 
-		final List<String> messages = plugin.getPlayerChecker()
-				.getRequirementsInStringList(
-						changeGroup.getRequirements(),
-						plugin.getPlayerChecker().getMetRequirements(
-								changeGroup.getRequirements(), player));
+		final List<String> messages = plugin.getPlayerChecker().getRequirementsInStringList(
+				changeGroup.getRequirements(),
+				plugin.getPlayerChecker().getMetRequirements(changeGroup.getRequirements(), player));
 
 		for (final String message : messages) {
 			AutorankTools.sendColoredMessage(sender, message);
 		}
 
-		sender.sendMessage(ChatColor.GREEN + "Preview of path '"
-				+ ChatColor.GRAY + pathName + ChatColor.GREEN + "'");
+		sender.sendMessage(ChatColor.GREEN + "Preview of path '" + ChatColor.GRAY + pathName + ChatColor.GREEN + "'");
 
 		return true;
 	}
 
-	/* (non-Javadoc)
-	 * @see me.armar.plugins.autorank.commands.manager.AutorankCommand#onTabComplete(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * me.armar.plugins.autorank.commands.manager.AutorankCommand#onTabComplete(
+	 * org.bukkit.command.CommandSender, org.bukkit.command.Command,
+	 * java.lang.String, java.lang.String[])
 	 */
 	@Override
-	public List<String> onTabComplete(final CommandSender sender,
-			final Command cmd, final String commandLabel, final String[] args) {
+	public List<String> onTabComplete(final CommandSender sender, final Command cmd, final String commandLabel,
+			final String[] args) {
 		// TODO Auto-generated method stub
 		final Player player = (Player) sender;
 
 		final List<String> possibilities = new ArrayList<String>();
 
-		final String groupName = plugin.getPermPlugHandler().getPrimaryGroup(
-				player);
+		final String groupName = plugin.getPermPlugHandler().getPrimaryGroup(player);
 
-		final List<ChangeGroup> changeGroups = plugin.getPlayerChecker()
-				.getChangeGroupManager().getChangeGroups(groupName);
+		final List<ChangeGroup> changeGroups = plugin.getPlayerChecker().getChangeGroupManager()
+				.getChangeGroups(groupName);
 
 		// List shows a list of changegroups to view
 		possibilities.add("list");
