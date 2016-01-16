81c70b2676b0d468caba255810cf58526fbc9755
Staartvin <mijnleraar@msn.com>
2016-01-16 13:56:46 +0100

    Formatting.

--- src/me/armar/plugins/autorank/data/SimpleYamlConfiguration.java
+++ src/me/armar/plugins/autorank/data/SimpleYamlConfiguration.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ConcurrentModificationException;
 import java.util.LinkedHashMap;
-
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,24 +13,20 @@ public class SimpleYamlConfiguration extends YamlConfiguration {
 
 	File file;
 
-	public SimpleYamlConfiguration(final JavaPlugin plugin,
-			final String fileName,
-			final LinkedHashMap<String, Object> configDefaults,
-			final String name) {
+	public SimpleYamlConfiguration(final JavaPlugin plugin, final String fileName,
+			final LinkedHashMap<String, Object> configDefaults, final String name) {
 		/*
 		 * accepts null as configDefaults -> check for resource and copies it if
 		 * found, makes an empty config if nothing is found
 		 */
-		final String folderPath = plugin.getDataFolder().getAbsolutePath()
-				+ File.separator;
+		final String folderPath = plugin.getDataFolder().getAbsolutePath() + File.separator;
 		file = new File(folderPath + fileName);
 
 		if (file.exists() == false) {
 			if (configDefaults == null) {
 				if (plugin.getResource(fileName) != null) {
 					plugin.saveResource(fileName, false);
-					plugin.getLogger().info(
-							"New " + name + " file copied from jar");
+					plugin.getLogger().info("New " + name + " file copied from jar");
 					try {
 						this.load(file);
 					} catch (final Exception e) {
