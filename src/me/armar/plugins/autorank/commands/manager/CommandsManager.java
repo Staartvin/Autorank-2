03d35c2187dc6ff3026becb5d5957564519f6cf2
Staartvin <mijnleraar@msn.com>
2017-02-04 14:58:18 +0100

    Remove redudant import.

--- src/me/armar/plugins/autorank/commands/manager/CommandsManager.java
+++ src/me/armar/plugins/autorank/commands/manager/CommandsManager.java
@@ -2,7 +2,6 @@ package me.armar.plugins.autorank.commands.manager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
