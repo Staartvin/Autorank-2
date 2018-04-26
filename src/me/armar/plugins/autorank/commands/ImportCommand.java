fb45dee752678c8e140b123c13c65c2d66b410d1
Staartvin <mijnleraar@msn.com>
2018-04-26 21:59:26 +0200

    Allow admins to import vanilla data of players from Minecraft.

--- src/me/armar/plugins/autorank/commands/ImportCommand.java
+++ src/me/armar/plugins/autorank/commands/ImportCommand.java
@@ -5,8 +5,11 @@ import me.armar.plugins.autorank.commands.manager.AutorankCommand;
 import me.armar.plugins.autorank.language.Lang;
 import me.armar.plugins.autorank.permissions.AutorankPermission;
 import me.armar.plugins.autorank.storage.StorageProvider;
+import me.armar.plugins.autorank.storage.TimeType;
 import me.armar.plugins.autorank.util.AutorankTools;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -28,10 +31,31 @@ public class ImportCommand extends AutorankCommand {
             return true;
         }
 
-        AutorankTools.sendColoredMessage(sender, Lang.DATA_IMPORTED.getConfigValue());
+        // Import data from vanilla minecraft
+        if (args.length > 1 && args[1] != null && args[1].equalsIgnoreCase("vanilladata")) {
+
+            int importedPlayers = 0;
+
+            for (OfflinePlayer offlinePlayer : plugin.getServer().getOfflinePlayers()) {
+
+                if (!offlinePlayer.hasPlayedBefore() || offlinePlayer.getPlayer() == null) continue;
+
+                // Time in seconds
+                int vanillaTime = offlinePlayer.getPlayer().getStatistic(Statistic.PLAY_ONE_TICK) / 20;
+
+                plugin.getPlayTimeManager().addLocalPlayTime(TimeType.TOTAL_TIME, offlinePlayer.getUniqueId(),
+                        vanillaTime / 60);
+
+                importedPlayers++;
+            }
+
+            sender.sendMessage(ChatColor.GREEN + "Imported data of " + importedPlayers + " players from Minecraft " +
+                    "statistics!");
+
+            return true;
+        }
 
         int supportingProviders = 0;
-
 
         // Check whether there is a storage provide that supports importing
         for (String namedStorageProvider : plugin.getStorageManager().getActiveStorageProviders()) {
@@ -45,12 +69,15 @@ public class ImportCommand extends AutorankCommand {
         if (supportingProviders <= 0) {
             sender.sendMessage(ChatColor.RED + "There is no active storage provider that allows importing of " +
                     "storage, hence Autorank cannot import data.");
+            return true;
         }
-
-        plugin.getStorageManager().importDataForStorageProviders();
 
         sender.sendMessage(ChatColor.GREEN + "Started importing data for " + ChatColor.GOLD +
                 supportingProviders + ChatColor.GREEN + " active storage providers.");
+
+        plugin.getStorageManager().importDataForStorageProviders();
+
+        AutorankTools.sendColoredMessage(sender, Lang.DATA_IMPORTED.getConfigValue());
 
         return true;
     }
