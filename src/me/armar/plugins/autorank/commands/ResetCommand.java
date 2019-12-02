4b6289fc7239555b58b6be0d642b58aa12d686be
Staartvin <mijnleraar@msn.com>
2019-12-02 18:34:44 +0100

    Start working on re-implementing reset command using conversations.

--- src/me/armar/plugins/autorank/commands/ResetCommand.java
+++ src/me/armar/plugins/autorank/commands/ResetCommand.java
@@ -1,16 +1,15 @@
 package me.armar.plugins.autorank.commands;
 
 import me.armar.plugins.autorank.Autorank;
+import me.armar.plugins.autorank.commands.conversations.AutorankConversation;
+import me.armar.plugins.autorank.commands.conversations.resetcommand.ResetConversation;
 import me.armar.plugins.autorank.commands.manager.AutorankCommand;
-import me.armar.plugins.autorank.language.Lang;
 import me.armar.plugins.autorank.permissions.AutorankPermission;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * The command delegator for the '/ar reset' command.
@@ -30,40 +29,53 @@ public class ResetCommand extends AutorankCommand {
             return true;
         }
 
-        if (args.length != 3) {
+        AutorankConversation resetConversation = AutorankConversation.fromFirstPrompt(new ResetConversation());
 
-            sender.sendMessage(Lang.INVALID_FORMAT.getConfigValue("/ar reset <player> <action>"));
-            return true;
-        }
+        resetConversation.afterConversationEnded(result -> {
+            System.out.println("Conversation ended and was successful: " + result.wasSuccessful());
+        });
 
-        final String target = args[1];
-        final String action = args[2];
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                resetConversation.startConversationAsSender(sender);
+            }
+        });
 
-        final UUID uuid = plugin.getUUIDStorage().getStoredUUID(target);
+//        if (args.length != 3) {
+//
+//            sender.sendMessage(Lang.INVALID_FORMAT.getConfigValue("/ar reset <player> <action>"));
+//            return true;
+//        }
 
-        if (uuid == null) {
-            sender.sendMessage(Lang.PLAYER_IS_INVALID.getConfigValue(target));
-            return true;
-        }
-
-        final String realName = plugin.getUUIDStorage().getRealName(uuid);
-
-        if (action.equalsIgnoreCase("progress")) {
-            // Reset progress of active paths.
-            plugin.getPathManager().resetProgressOnActivePaths(uuid);
-            sender.sendMessage(ChatColor.GREEN + "Reset progress on all active paths of " + ChatColor.YELLOW +
-                    realName);
-        } else if (action.equalsIgnoreCase("activepaths")) {
-            plugin.getPathManager().resetActivePaths(uuid);
-            sender.sendMessage(ChatColor.GREEN + "Removed all active paths of " + ChatColor.YELLOW + realName);
-        } else if (action.equalsIgnoreCase("completedpaths")) {
-            plugin.getPathManager().resetCompletedPaths(uuid);
-            sender.sendMessage(ChatColor.GREEN + "Removed all completed paths of " + ChatColor.YELLOW + realName);
-        } else {
-            sender.sendMessage(ChatColor.RED + "Invalid action. You can only use: progress, activepaths or " +
-                    "completedpaths.");
-            return true;
-        }
+//        final String target = args[1];
+//        final String action = args[2];
+//
+//        final UUID uuid = plugin.getUUIDStorage().getStoredUUID(target);
+//
+//        if (uuid == null) {
+//            sender.sendMessage(Lang.PLAYER_IS_INVALID.getConfigValue(target));
+//            return true;
+//        }
+//
+//        final String realName = plugin.getUUIDStorage().getRealName(uuid);
+//
+//        if (action.equalsIgnoreCase("progress")) {
+//            // Reset progress of active paths.
+//            plugin.getPathManager().resetProgressOnActivePaths(uuid);
+//            sender.sendMessage(ChatColor.GREEN + "Reset progress on all active paths of " + ChatColor.YELLOW +
+//                    realName);
+//        } else if (action.equalsIgnoreCase("activepaths")) {
+//            plugin.getPathManager().resetActivePaths(uuid);
+//            sender.sendMessage(ChatColor.GREEN + "Removed all active paths of " + ChatColor.YELLOW + realName);
+//        } else if (action.equalsIgnoreCase("completedpaths")) {
+//            plugin.getPathManager().resetCompletedPaths(uuid);
+//            sender.sendMessage(ChatColor.GREEN + "Removed all completed paths of " + ChatColor.YELLOW + realName);
+//        } else {
+//            sender.sendMessage(ChatColor.RED + "Invalid action. You can only use: progress, activepaths or " +
+//                    "completedpaths.");
+//            return true;
+//        }
 
         return true;
     }
